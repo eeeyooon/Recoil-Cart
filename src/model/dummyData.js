7244be6f47df5f529b5bad54e81cbd83bb6e20c4
jiyoon <vywns9978@gmail.com>
2023-07-15 16:06:06 +0900

    [⚙️ chore] 더미데이터 추가

---
+++ src/model/dummyData.js
@@ -0,0 +1,39 @@
+const dummyData = [
+  {
+    id: 1,
+    price: 130000,
+    title: "[리코일] Recoil made in USA",
+    description: "리코일은 정말 좋은 상태관리 라이브러리에요",
+  },
+  {
+    id: 2,
+    price: 90000,
+    title: "[리액트] React is good",
+    description: "리액트가 솔직히 짱이죠",
+  },
+  {
+    id: 3,
+    price: 1000,
+    title: "[푸바오] 이것저것 좋아하는 푸바오",
+    description: "용인시 털주먹 뚠뚠 빵빵한 푸린세스",
+  },
+  {
+    id: 4,
+    price: 1390000,
+    title: "[루피] loopy made in Korea",
+    description: "여러분의 개발자라이프 응원할게요!",
+  },
+  {
+    id: 5,
+    price: 90000,
+    title: "[리액트] React is good",
+    description: "리액트가 솔직히 짱이죠",
+  },
+  {
+    id: 6,
+    price: 1390000,
+    title: "[루피] loopy made in Korea",
+    description: "여러분의 개발자라이프 응원할게요!",
+  },
+];
+export default dummyData;
