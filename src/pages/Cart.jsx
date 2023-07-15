5778aa33c10ee7fdd246573359266cc2eb5665ac
jiyoon <vywns9978@gmail.com>
2023-07-15 16:11:04 +0900

    [✨ feat] recoil을 사용하여 상태 관리 추가

---
+++ src/pages/Cart.jsx
@@ -0,0 +1,84 @@
+import { useRecoilValue } from "recoil";
+import {
+  CartAtom,
+  TotalPriceSelector,
+  TotalQuantitySelector,
+} from "../recoil/CartAtom";
+import styled from "styled-components";
+import CartItem from "./../component/CartItem/CartItem";
+
+function Cart() {
+  // 전역 상태 관리소의 값을 불러오기
+  const cartItem = useRecoilValue(CartAtom);
+
+  //? 파생 데이터인 셀렉터를 이용하기
+
+  // 총 수량
+  const TotalQuantity = useRecoilValue(TotalQuantitySelector);
+
+  // 총액
+  const TotalPrice = useRecoilValue(TotalPriceSelector);
+
+  return (
+    <>
+      <Heading>장바구니</Heading>
+
+      <ItemWrapper>
+        {cartItem.length ? (
+          cartItem.map((e) => <CartItem data={e} key={e.id} />)
+        ) : (
+          <NoItems>상품이 없습니다</NoItems>
+        )}
+      </ItemWrapper>
+
+      <TotalPriceWrapper>
+        <ColumnWrapper>
+          <span>총 갯수</span>
+          <Heading>{`${TotalQuantity}개`}</Heading>
+        </ColumnWrapper>
+        <ColumnWrapper>
+          <span>총 가격</span>
+          <Heading>{`${TotalPrice.toLocaleString()}원`}</Heading>
+        </ColumnWrapper>
+      </TotalPriceWrapper>
+    </>
+  );
+}
+const ColumnWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 16px;
+`;
+
+const Heading = styled.span`
+  font-size: 20px;
+  font-weight: var(--bold);
+`;
+const ItemWrapper = styled.ul`
+  display: flex;
+  width: 100%;
+  height: 100%;
+  min-height: calc(100vh - 300px);
+  gap: 8px;
+  flex-direction: column;
+`;
+const TotalPriceWrapper = styled.div`
+  padding: 16px;
+  height: 150px;
+  width: 100%;
+  max-width: 1024px;
+  border: 1px solid var(--line-gray);
+  & span {
+    text-align: right;
+  }
+`;
+const NoItems = styled.div`
+  padding: 8px;
+  width: fit-content;
+  margin: 0 auto;
+  border-radius: 4px;
+  text-align: center;
+  border: 1px solid var(--line-gray);
+`;
+
+export default Cart;
